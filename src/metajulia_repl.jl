e3404e58a9ce990f7ae5e548f45bec6d5ced0a82
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-07 09:23:29 +0000

    feat: add basic call expressions

--- src/metajulia_repl.jl
+++ src/metajulia_repl.jl
@@ -1,6 +1,8 @@
 function evaluate(expr)
     if self_evaluating(expr)
         expr
+    elseif is_call(expr)
+        evaluate_call(expr)
     else
         throw("Not implemented")
     end
@@ -8,6 +10,26 @@ end
 
 function self_evaluating(expr)
     isa(expr, Int) || isa(expr, Bool) || isa(expr, String)
+end
+
+function is_call(expr)
+    isa(expr, Expr) && expr.head == :call
+end
+
+function evaluate_call(expr)
+    func = expr.args[1]
+    args = map(evaluate, expr.args[2:end])
+    if func == :+
+        sum(args)
+    elseif func == :*
+        prod(args)
+    elseif func == :>
+        args[1] > args[2]
+    elseif func == :<
+        args[1] < args[2]
+    else
+        throw("Not implemented (EVALUATE_CALL)")
+    end
 end
 
 function metajulia_repl()
