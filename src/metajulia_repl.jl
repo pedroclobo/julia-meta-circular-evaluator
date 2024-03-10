f4b03689d96a08e38ad12851b963f728f28a42a1
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-10 11:06:25 +0000

    feat: parse multi-line expressions

--- src/metajulia_repl.jl
+++ src/metajulia_repl.jl
@@ -143,6 +143,12 @@ eval_assignment(expr, env) = (modify_env!(env, definition_name(expr), eval(defin
 function metajulia_repl()
     while true
         print(">> ")
-        println(eval(Meta.parse(readline()), empty_env()))
+        expr = readline()
+        parsed_expr = Meta.parse(raise=false, expr)
+        while parsed_expr.head == :incomplete || parsed_expr.head == :error
+            expr = string(expr, '\n', readline())
+            parsed_expr = Meta.parse(raise=false, expr)
+        end
+        println(eval(Meta.parse(expr), empty_env()))
     end
 end
