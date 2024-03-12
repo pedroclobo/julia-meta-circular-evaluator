280f39016693bbe84b53d59c9fdf9ee8a078147f
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-12 17:09:02 +0000

    fix: repl throws error when evaluating non Expr

--- src/metajulia_repl.jl
+++ src/metajulia_repl.jl
@@ -1,11 +1,15 @@
 include("evaluator.jl")
 
+is_valid_expr(expr) =
+    !isa(expr, Expr) ||
+    (isa(expr, Expr) && expr.head != :incomplete && expr.head != :error)
+
 function metajulia_repl()
     while true
         print(">> ")
         expr = readline()
         parsed_expr = Meta.parse(raise=false, expr)
-        while parsed_expr.head == :incomplete || parsed_expr.head == :error
+        while !is_valid_expr(parsed_expr)
             expr = string(expr, '\n', readline())
             parsed_expr = Meta.parse(raise=false, expr)
         end
