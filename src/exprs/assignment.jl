6760fc534a5c9ea6e2aeeba0f4aaaf0244c6297a
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-14 19:37:31 +0000

    refactor: join function and other assignments/declarations

--- src/exprs/assignment.jl
+++ src/exprs/assignment.jl
@@ -1,27 +1,19 @@
 is_assignment(expr, env) =
-    isa(expr, Expr) && expr.head == :(=) && isa(expr.args[1], Symbol) &&
-    has_name_in_frame(expr.args[1], env)
+    isa(expr, Expr) && expr.head == :(=) && (
+        (isa(expr.args[1], Symbol) && has_name_in_frame(expr.args[1], env)) ||
+        ((isa(expr.args[1], Expr) && has_name_in_frame(expr.args[1].args[1], env)))
+    )
 
-assignment_name(expr) = expr.args[1]
+assignment_name(expr) = is_call(expr.args[1]) ? expr.args[1].args[1] : expr.args[1]
 
-assignment_init(expr) = expr.args[2]
+assignment_init(expr, env) = is_call(expr.args[1]) ?
+                        make_lambda(expr.args[1].args[2:end], expr.args[2], env) :
+                        expr.args[2]
 
 eval_assignment(expr, env) =
-    let init = eval(assignment_init(expr), env)
+    let init = eval(assignment_init(expr, env), env)
         # Destructively add a new binding to the last frame of the environment
         # (note the !)
         add_binding!(env, assignment_name(expr), init)
         init
     end
-
-is_function_assignment(expr, env) =
-    isa(expr, Expr) && expr.head == :(=) && isa(expr.args[1], Expr) &&
-    has_name(expr.args[1], env)
-
-function_assignment_name(expr) = expr.args[1].args[1]
-
-function_assignment_init(expr, env) = make_lambda(expr.args[1].args[2:end], expr.args[2], env)
-
-# A function assignment is the assignment of a lambda expression
-eval_function_assignment(expr, env) =
-    eval_assignment(:($(function_assignment_name(expr)) = $(function_assignment_init(expr))), env)
