02ab36c08fedfd49ad2164ae12475a6540fe22c1
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-14 19:37:31 +0000

    refactor: join function and other assignments/declarations

--- src/exprs/definition.jl
+++ src/exprs/definition.jl
@@ -1,27 +1,19 @@
 is_definition(expr, env) =
-    isa(expr, Expr) && expr.head == :(=) && isa(expr.args[1], Symbol) &&
-    !has_name_in_frame(expr.args[1], env)
+    isa(expr, Expr) && expr.head == :(=) && (
+        (isa(expr.args[1], Symbol) && !has_name_in_frame(expr.args[1], env)) ||
+        ((isa(expr.args[1], Expr) && !has_name_in_frame(expr.args[1].args[1], env)))
+    )
 
-definition_name(expr) = expr.args[1]
+definition_name(expr) = is_call(expr.args[1]) ? expr.args[1].args[1] : expr.args[1]
 
-definition_init(expr) = expr.args[2]
+definition_init(expr, env) = is_call(expr.args[1]) ?
+                             make_lambda(expr.args[1].args[2:end], expr.args[2], env) :
+                             expr.args[2]
 
 eval_definition(expr, env) =
-    let init = eval(definition_init(expr), env)
+    let init = eval(definition_init(expr, env), env)
         # Destructively add a new binding to the last frame of the environment
         # (note the !)
         add_binding!(env, definition_name(expr), init)
         init
     end
-
-is_function_definition(expr, env) =
-    isa(expr, Expr) && expr.head == :(=) && isa(expr.args[1], Expr) &&
-    !has_name(expr.args[1], env)
-
-function_definition_name(expr) = expr.args[1].args[1]
-
-function_definition_init(expr, env) = make_lambda(expr.args[1].args[2:end], expr.args[2], env)
-
-# A function definition is the definition of a lambda expression
-eval_function_definition(expr, env) =
-    eval_definition(:($(function_definition_name(expr)) = $(function_definition_init(expr, env))), env)
