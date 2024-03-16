4463386b4a79a7c7906eadcc43dfde9d51d381fc
Rómulo Baltazar Nicolas Kaidussis <romulokaidussis@tecnico.ulisboa.pt>
2024-03-16 16:39:27 +0000

    feat: implement global assignment/definition

--- src/exprs/assignment.jl
+++ src/exprs/assignment.jl
@@ -17,3 +17,23 @@ eval_assignment(expr, env) =
         add_binding!(env, assignment_name(expr), init)
         init
     end
+
+is_global_assignment(expr, env) = isa(expr, Expr) && expr.head == :global &&
+    isa(expr.args[1], Expr) && expr.args[1].head == :(=) && (
+        (isa(expr.args[1].args[1], Symbol) && has_name_in_global_frame(expr.args[1].args[1], env)) ||
+        ((isa(expr.args[1].args[1], Expr) && has_name_in_global_frame(expr.args[1].args[1].args[1], env)))
+    )
+
+global_assignment_name(expr) = assignment_name(expr.args[1])
+
+global_assignment_init(expr, env) = assignment_init(expr.args[1], env)
+
+eval_global_assignment(expr, env) =
+    begin
+        let init = eval(global_assignment_init(expr, env), env)
+            # Destructively update a binding to the global frame of the environment
+            # (note the !)
+            add_binding_to_global_frame!(env, global_assignment_name(expr), init)
+            init
+        end
+    end 
--- src/exprs/definition.jl
+++ src/exprs/definition.jl
@@ -11,9 +11,33 @@ definition_init(expr, env) = is_call(expr.args[1]) ?
                              expr.args[2]
 
 eval_definition(expr, env) =
-    let init = eval(definition_init(expr, env), env)
-        # Destructively add a new binding to the last frame of the environment
-        # (note the !)
-        add_binding!(env, definition_name(expr), init)
-        init
+    begin
+        let init = eval(definition_init(expr, env), env)
+            # Destructively add a new binding to the last frame of the environment
+            # (note the !)
+            add_binding!(env, definition_name(expr), init)
+            init
+        end
     end
+
+
+is_global_definition(expr, env) = 
+    isa(expr, Expr) && expr.head == :global &&
+    isa(expr.args[1], Expr) && expr.args[1].head == :(=) && (
+        (isa(expr.args[1].args[1], Symbol) && !has_name_in_global_frame(expr.args[1].args[1], env)) ||
+        ((isa(expr.args[1].args[1], Expr) && !has_name_in_global_frame(expr.args[1].args[1].args[1], env)))
+    )
+
+global_definition_name(expr) = definition_name(expr.args[1])
+
+global_definition_init(expr, env) = definition_init(expr.args[1], env)
+
+eval_global_definition(expr, env) =
+    begin
+        let init = eval(global_definition_init(expr, env), env)
+            # Destructively add a new binding to the global frame of the environment
+            # (note the !)
+            add_binding_to_global_frame!(env, global_definition_name(expr), init)
+            init
+        end
+    end 
