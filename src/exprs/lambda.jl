69475ba84c4ed0ba541b08f439eb2d8f02425a38
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-19 18:25:26 +0000

    refactor: join assignments and definitions

--- src/exprs/lambda.jl
+++ src/exprs/lambda.jl
@@ -1,5 +1,3 @@
-include("fexpr.jl")
-
 #=
 Function captures a lambda and the environment where it was defined.
 The environment is need to support lexical scoping, as the environment
@@ -31,29 +29,3 @@ lambda_body(expr) = expr.args[2]
 make_lambda(args, body, env) = Function(:($(Expr(:tuple, (args...))) -> $(body.args[2])), env)
 
 eval_lambda(expr, env) = make_lambda(lambda_params(expr), lambda_body(expr), env)
-
-
-is_call(expr) = isa(expr, Expr) && expr.head == :call
-call_op(call) = call.args[1]
-call_args(call) = call.args[2:end]
-
-eval_call(call, env) =
-    let f = eval(call_op(call), env)
-        # If the calee is a function, extend the function's environment
-        # and evaluate its body
-        if is_function(f)
-            let (lambda, lambda_env, args) = (function_lambda(f), function_env(f), eval_exprs(call_args(call), env))
-                extend_env!(lambda_env, lambda_params(lambda), args)
-                eval(lambda_body(lambda), lambda_env)
-            end
-        elseif is_fexpr(f)
-            let (lambda, lambda_env) = (function_lambda(f), function_env(f))
-                extend_env!(lambda_env, lambda_params(lambda), call_args(call))
-                eval(lambda_body(lambda), lambda_env)
-            end
-        else
-            let (args = eval_exprs(call_args(call), env))
-                f(args...)
-            end
-        end
-    end
