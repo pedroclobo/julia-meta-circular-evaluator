6deeb9bd106736bfa6b953bd6091ffda267a2173
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-21 14:26:22 +0000

    refactor: use show instead of custom print function

--- src/metajulia_repl.jl
+++ src/metajulia_repl.jl
@@ -6,17 +6,6 @@ is_valid_expr(expr) =
 
 metajulia_eval(expr) = eval(expr, empty_env())
 
-print_expr(expr) =
-    if isa(expr, Expr)
-        println(":($expr)")
-    elseif isa(expr, String)
-        println("\"$expr\"")
-    elseif isa(expr, Symbol)
-        println(":$expr")
-    else
-        println(expr)
-    end
-
 metajulia_repl() =
     while true
         print(">> ")
@@ -27,5 +16,6 @@ metajulia_repl() =
             parsed_expr = Meta.parse(raise=false, expr)
         end
         expr = eval(Meta.parse(expr), empty_env())
-        print_expr(expr)
+        show(expr)
+        println()
     end
