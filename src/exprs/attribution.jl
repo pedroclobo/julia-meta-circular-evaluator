371e88c0079609ef79d311f1db587c728f08b4e5
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-22 14:30:02 +0000

    fix: local attributions can shadow higher local frames

--- src/exprs/attribution.jl
+++ src/exprs/attribution.jl
@@ -53,17 +53,17 @@ init(expr, env) =
 is_definition(expr, env) = is_local_definition(expr, env) || is_global_definition(expr, env)
 
 is_local_definition(expr, env) =
-    (is_variable_attribution(expr) && !has_name_in_frame(expr.args[1], env)) ||
-    (is_function_attribution(expr) && !has_name_in_frame(expr.args[1].args[1], env)) ||
-    (is_fexpr_attribution(expr) && !has_name_in_frame(expr.args[1].args[1], env)) ||
-    (is_macro_attribution(expr) && !has_name_in_frame(expr.args[1].args[1], env))
+    (is_variable_attribution(expr) && !has_name_in_local_frames(expr.args[1], env)) ||
+    (is_function_attribution(expr) && !has_name_in_local_frames(expr.args[1].args[1], env)) ||
+    (is_fexpr_attribution(expr) && !has_name_in_local_frames(expr.args[1].args[1], env)) ||
+    (is_macro_attribution(expr) && !has_name_in_local_frames(expr.args[1].args[1], env))
 
 is_global_definition(expr, env) =
     isa(expr, Expr) && expr.head == :global &&
         (is_variable_attribution(expr.args[1]) && !has_name_in_global_frame(expr.args[1].args[1], env)) ||
         (is_function_attribution(expr.args[1]) && !has_name_in_global_frame(expr.args[1].args[1].args[1], env)) ||
         (is_fexpr_attribution(expr.args[1]) && !has_name_in_global_frame(expr.args[1].args[1].args[1], env)) ||
-        (is_macro_attribution(expr.args[1]) && !has_name_in_frame(expr.args[1].args[1].args[1], env))
+        (is_macro_attribution(expr.args[1]) && !has_name_in_global_frame(expr.args[1].args[1].args[1], env))
 
 eval_definition(expr, env) =
     begin
@@ -80,10 +80,10 @@ eval_definition(expr, env) =
 is_assignment(expr, env) = is_local_assignment(expr, env) || is_global_assignment(expr, env)
 
 is_local_assignment(expr, env) =
-    (is_variable_attribution(expr) && has_name_in_frame(expr.args[1], env)) ||
-    (is_function_attribution(expr) && has_name_in_frame(expr.args[1].args[1], env)) ||
-    (is_fexpr_attribution(expr) && has_name_in_frame(expr.args[1].args[1], env)) ||
-    (is_macro_attribution(expr) && has_name_in_frame(expr.args[1].args[1], env))
+    (is_variable_attribution(expr) && has_name_in_local_frames(expr.args[1], env)) ||
+    (is_function_attribution(expr) && has_name_in_local_frames(expr.args[1].args[1], env)) ||
+    (is_fexpr_attribution(expr) && has_name_in_local_frames(expr.args[1].args[1], env)) ||
+    (is_macro_attribution(expr) && has_name_in_local_frames(expr.args[1].args[1], env))
 
 is_global_assignment(expr, env) =
     isa(expr, Expr) && expr.head == :global &&
@@ -96,7 +96,7 @@ eval_assignment(expr, env) =
     begin
         let init = eval(init(expr, env), env)
             if is_local_assignment(expr, env)
-                add_binding!(env, name(expr, env), init)
+                replace_binding!(env, name(expr, env), init)
             elseif is_global_assignment(expr, env)
                 add_binding_to_global_frame!(env, name(expr, env), init)
             end
